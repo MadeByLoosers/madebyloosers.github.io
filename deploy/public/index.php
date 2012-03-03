e9aea29da05a1d20b20fed42c1a38c5ca7a8850a
John Lund <john@lundesign.co.uk>
2012-03-03 18:32:35 +0000

    fixed #47 Removed html comments

--- deploy/public/index.php
+++ deploy/public/index.php
@@ -18,39 +18,21 @@
 
 ?>
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
 
-  <!-- Use the .htaccess and remove these lines to avoid edge case issues.
-       More info: h5bp.com/b/378 -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
   <title>Gunt London - <?php echo $tagLine; ?></title>
   <meta name="description" content="">
   <meta name="author" content="Captain Gunt">
-
-  <!-- Mobile viewport optimized: j.mp/bplateviewport -->
   <meta name="viewport" content="width=device-width,initial-scale=1">
-
-  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
-
-  <!-- CSS: implied media=all -->
-  <!-- CSS concatenated and minified via ant build script-->
   <link rel="stylesheet" href="includes/css/style.css">
   <link rel="stylesheet" href="includes/css/flexslider.css" type="text/css">
-  <!-- end CSS-->
-
-  <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
-
-  <!-- All JavaScript at the bottom, except for Modernizr / Respond.
-       Modernizr enables HTML5 elements & feature detects; Respond is a polyfill for min/max-width CSS3 Media Queries
-       For optimal performance, use a custom Modernizr build: www.modernizr.com/download/ -->
   <script src="includes/js/libs/modernizr-2.0.6.min.js"></script>
 </head>
 
@@ -288,7 +270,7 @@
     </div>
      
   </div>
-</div> <!--! end of #container -->
+</div>
 
   <footer>
     <div class="footer-container">
@@ -307,25 +289,16 @@
     </div>
   </footer>
 
-
-  <!-- JavaScript at the bottom for fast page loading -->
-
-  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="includes/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 
-
-  <!-- scripts concatenated and minified via ant build script-->
   <script defer src="includes/js/plugins.js"></script>
-    <script defer src="includes/js/mylibs/raphael.js"></script>
+  <script defer src="includes/js/mylibs/raphael.js"></script>
   <script defer src="includes/js/mylibs/jquery.smoothscroll.js"></script>
-    <script defer src="includes/js/mylibs/jquery.flexslider.min.js"></script>
-    <script defer src="includes/js/mylibs/waypoints.min.js"></script>
+  <script defer src="includes/js/mylibs/jquery.flexslider.min.js"></script>
+  <script defer src="includes/js/mylibs/waypoints.min.js"></script>
   <script defer src="includes/js/script.js"></script>
-  <!-- end scripts-->
-
-	
-  <!-- Change UA-XXXXX-X to be your site's ID -->
+  
   <script>
     window._gaq = [['_setAccount','UA-29684348-1'],['_trackPageview'],['_trackPageLoadTime']];
     Modernizr.load({
@@ -333,13 +306,9 @@
     });
   </script>
 
-
-  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
-       chromium.org/developers/how-tos/chrome-frame-getting-started -->
   <!--[if lt IE 7 ]>
     <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
     <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-  <![endif]-->
-  
+  <![endif]-->  
 </body>
 </html>
